7eb0c914625bb36d6537048988976a7f596b2912
sericaer <sericaer@outlook.com>
2022-01-17 14:24:08 +0800

    add TextInput Bind

---
+++ Assets/Scenes/TextInputBind/TextInputBind.cs
@@ -0,0 +1,55 @@
+using FairyGUI;
+using FairyGUI.DataBind;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class TextInputBind : MonoBehaviour
+{
+    TestData testData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        testData = new TestData();
+
+        UIPackage.AddPackage("TextInputBind");
+
+        var gComponent = UIPackage.CreateObject("TextInputBind", "Component1").asCom;
+        gComponent.BindDataSource(testData);
+
+        GRoot.inst.AddChild(gComponent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    class TestData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string input
+        {
+            get
+            {
+                return _input;
+            }
+            set
+            {
+                _input = value;
+                Debug.Log($"input changed to {_input}");
+            }
+        }
+
+        private string _input;
+
+        public TestData()
+        {
+            input = "INIT";
+        }
+    }
+}
